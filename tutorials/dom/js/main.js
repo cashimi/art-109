aed573bcd6de9f85032e7689bb1cdf1de8303849
cashimi <caitlynvu@gmail.com>
2025-09-28 23:54:25 -0700

    some tut 5

---
+++ tutorials/dom/js/main.js
@@ -0,0 +1,19 @@
+
+console.log("hello hello");
+
+
+let pageTitle = document.querySelector("#page-title")
+
+// Javascript Timeout changes h1 tilte after 3 seconds
+setTimeout(function(){
+    pageTitle.style.color = "pink";
+    // console.log("timeout worked!");
+},3000);
+
+// Click event on header changes background color
+document.querySelector("header").onclick = function() {
+    // console.log("clicked header");
+    document.querySelector("body").style.backgroundColor = "black";
+}
+
+
