4431b44c5d2ae1acca59a377527f6b9b89b858a5
cashimi <caitlynvu@gmail.com>
2025-09-29 11:31:13 -0700

    remainder of tut 5
    my residents jumped me and i couldn't finish on time...

--- tutorials/dom/js/main.js
+++ tutorials/dom/js/main.js
@@ -1,19 +1,46 @@
 
-console.log("hello hello");
+document.querySelector("#image-0").addEventListener("click", function(){
+    document.querySelector("#image-1").style.visibility = "visible";
+    alert("bark bark bark");
+})
+document.querySelector("#image-1").addEventListener("click", function(){
+    document.querySelector("#image-2").style.visibility = "visible";
+})
+document.querySelector("#image-2").addEventListener("click", function(){
+    document.querySelector("#image-3").style.visibility = "visible";
+})
+document.querySelector("#image-3").addEventListener("click", function(){
+    document.querySelector("#image-4").style.visibility = "visible";
+})
+document.querySelector("#image-4").addEventListener("click", function(){
+    document.querySelector("#image-5").style.visibility = "visible";
+})
+document.querySelector("#image-5").addEventListener("click", function(){
+    document.querySelector("#image-6").style.visibility = "visible";
+})
+document.querySelector("#image-6").addEventListener("click", function(){
+})
 
 
-let pageTitle = document.querySelector("#page-title")
 
-// Javascript Timeout changes h1 tilte after 3 seconds
-setTimeout(function(){
-    pageTitle.style.color = "pink";
-    // console.log("timeout worked!");
-},3000);
 
-// Click event on header changes background color
-document.querySelector("header").onclick = function() {
-    // console.log("clicked header");
-    document.querySelector("body").style.backgroundColor = "black";
-}
+
+//------------------------------------------------------------------- part one of JS tut
+// console.log("hello hello");
+
+
+// let pageTitle = document.querySelector("#page-title")
+
+// // Javascript Timeout changes h1 tilte after 3 seconds
+// setTimeout(function(){
+//     pageTitle.style.color = "pink";
+//     // console.log("timeout worked!");
+// },3000);
+
+// // Click event on header changes background color
+// document.querySelector("header").onclick = function() {
+//     // console.log("clicked header");
+//     document.querySelector("body").style.backgroundColor = "black";
+// }
 
 
